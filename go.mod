848f9a98b6d5282b0fcd87bf8c684561ab871f5e
Matheus Sousa <matessousa@outlook.com>
2024-02-22 23:36:27 -0300

    feat: added ls command and refactor existed methods

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
-module change-kubectl-ctx
+module kctx
 
 go 1.20
 
 require (
+	github.com/fatih/color v1.16.0
 	github.com/manifoldco/promptui v0.9.0
+	github.com/spf13/cobra v1.8.0
 	k8s.io/client-go v0.28.4
 )
 
@@ -15,13 +17,16 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/term v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
--- main.go
+++ main.go
@@ -3,50 +3,138 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	kubeconfig := os.Getenv("KUBECONFIG")
-	if kubeconfig == "" {
+	contexts, config, kubeconfig := LoadKubeConfig()
+
+	helper := `
+  Usage:
+    kubectx [command]
+  
+  Available Commands:
+    default Set the default Kubernetes context
+    ls  List Kubernetes Contexts
+    help  Help about any command
+
+  Flags:
+    -h, --help   help for kubectx commands
+
+  Use "kubectx [command] --help" for more information about a command.`
+
+	cmd := &cobra.Command{
+		Use:   "kubectx",
+		Short: "Switch Kubernetes Context",
+		Run: func(cmd *cobra.Command, args []string) {
+			ChangeContext(contexts, config, kubeconfig)
+		},
+	}
+
+	lsCmd := &cobra.Command{
+		Use:   "ls",
+		Short: "List Kubernetes Contexts",
+		Run: func(cmd *cobra.Command, args []string) {
+			ListContexts(contexts, config.CurrentContext)
+		},
+	}
+
+	cmd.AddCommand(lsCmd)
+	cmd.SetHelpTemplate(helper)
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func LoadKubeConfig() (contexts []string, config *api.Config, kubeconfig string) {
+	kubeconfigs := os.Getenv("KUBECONFIG")
+	if kubeconfigs == "" {
 		if home := homedir.HomeDir(); home != "" {
-			kubeconfig = home + "/.kube/config"
+			kubeconfigs = home + "/.kube/config"
 		} else {
 			fmt.Println("Unable to find kubeconfig file")
 			os.Exit(1)
 		}
 	}
-
-	config, err := clientcmd.LoadFromFile(kubeconfig)
+	config, err := clientcmd.LoadFromFile(kubeconfigs)
 	if err != nil {
 		fmt.Printf("Error loading kubeconfig: %s\n", err)
 		os.Exit(1)
 	}
 
-	var contexts []string
+	contexts = []string{}
+
 	for context := range config.Contexts {
 		contexts = append(contexts, context)
 	}
 
+	return contexts, config, kubeconfigs
+}
+
+func ChangeContext(contexts []string, config *api.Config, kubeconfig string) {
+	ctxs := make([]string, len(contexts))
+
+	green := color.New(color.FgGreen).SprintFunc()
+	currentContextIndicator := green(" <- current context")
+
+	for i, context := range contexts {
+		if context == config.CurrentContext {
+			ctxs[i] = context + currentContextIndicator
+		} else {
+			ctxs[i] = context
+		}
+	}
+
 	prompt := promptui.Select{
 		Label: "Select Kubernetes Context",
-		Items: contexts,
+		Items: ctxs,
+		Templates: &promptui.SelectTemplates{
+			Active:   `🚀 {{ . | green }}`,
+			Inactive: `{{ . }}`,
+			Selected: `{{ "✔" | green }} {{ . }}`,
+		},
 	}
 
 	_, selectedContext, err := prompt.Run()
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
+		if err == promptui.ErrInterrupt {
+			fmt.Printf("Goodbye 👋\n")
+			os.Exit(0)
+		} else {
+			fmt.Printf("Prompt selection failed: %v\n", err)
+			os.Exit(1)
+		}
 		return
 	}
+	selectedContextWithoutIndicator := strings.TrimSuffix(selectedContext, currentContextIndicator)
 
-	config.CurrentContext = selectedContext
+	config.CurrentContext = selectedContextWithoutIndicator
 	if err := clientcmd.WriteToFile(*config, kubeconfig); err != nil {
 		fmt.Printf("Error writing kubeconfig file: %s\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Context switched to %q\n", selectedContext)
+	green = color.New(color.FgGreen).SprintFunc()
+	fmt.Printf(green("Switched to context: %s\n"), selectedContext)
+	os.Exit(0)
+}
+
+func ListContexts(contexts []string, currentContext string) {
+	green := color.New(color.FgGreen).SprintFunc()
+	for _, context := range contexts {
+		if context == currentContext {
+			fmt.Println(context, green("<- current context"))
+		} else {
+			fmt.Println(context)
+		}
+	}
 }
